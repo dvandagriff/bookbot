d0750215863ff42a20f04c334009c180f7f1feee
dvandagriff <vandagriff.drew@icloud.com>
2025-06-10 17:23:25 -0500

    stats

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+from stats import get_num_words, get_sorted_num_chars
+
 def get_book_text(path_to_file: str) -> str:
     with open(path_to_file) as f:
         out = f.read()
     return out
 
 
-def get_num_words(book_str: str) -> int:
-    return sum([i for i in book_str.replace('  ', ' ').replace('\n', ' ').split(' ') if len(i) > 0])
+def print_report(book_path: str, book_str: str):
+    data = {
+        "word_count": get_num_words(book_str),
+        "char_count": [
+            f"{i['char']}: {i['num']}"
+            for i in get_sorted_num_chars(book_str)
+        ]
+    }
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {data['word_count']} total words")
+    print("--------- Character Count -------")
+    for i in data['char_count']:
+        print(i)
+    print("============= END ===============")
 
 
 def main():
-    num_words = get_num_words(get_book_text('books/frankenstein.txt'))
-    print(f"{num_words} words found in the document")
+    book_path = 'books/frankenstein.txt'
+    print_report(book_path, get_book_text(book_path))
 
 
 if __name__ == '__main__':
---
+++ stats.py
@@ -0,0 +1,24 @@
+from curses.ascii import isalpha
+def get_num_words(book_str: str) -> int:
+    return len([i for i in book_str.replace('  ', ' ').replace('\n', ' ').split(' ') if len(i) > 0])
+
+
+def get_num_chars(book_str: str) -> dict:
+    out = {}
+    for i in book_str:
+        _i = i.lower()
+        if _i not in out:
+            out[_i] = 1
+        else:
+            out[_i] += 1
+    return out
+
+
+def get_sorted_num_chars(book_str: str) -> list:
+    out = [
+        {'char': k, 'num': v}
+        for k,v in get_num_chars(book_str).items()
+        if k.isalpha()
+    ]
+    out.sort(reverse=True, key=lambda a: a['num'])
+    return out
